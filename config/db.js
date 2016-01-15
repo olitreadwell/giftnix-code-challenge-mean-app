1becf574003b5f7cb7638d70bce6fbf4767c6c00
olitreadwell <olitreadwell@gmail.com>
2016-01-15 11:46:38 -0600

    setup connections to mongolab db

---
+++ config/db.js
@@ -0,0 +1,5 @@
+// config/db.js
+
+  module.exports = {
+    url : "mongodb://olitreadwell:!dW5Rd722507B8Xu6210kqFw$35N49QCM5tu5NNsP#5964537J121FT27wk51gY!g1H4rVh554B4cNnG49uZm3C4LZy658jNJ7V@ds045785.mongolab.com:45785/giftnix-weather-mean-app-dev"
+  }
